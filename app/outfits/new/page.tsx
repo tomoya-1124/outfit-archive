a6bcb75b7ff93f397e30683749e40c5519b9b832
阿部 智成（AbeTomonari） <t_abe@umenetts.co.jp>
2026-05-08 13:56:20 +0900

    fix: add import part for fix deployment error

--- app/outfits/new/page.tsx
+++ app/outfits/new/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import {useState } from 'react';
 import { useRouter } from "next/navigation";
 import OutfitForm from "@/components/outfits/OutfitForm";
 import ErrorState from "@/components/ui/ErrorState";
